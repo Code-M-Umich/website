4ba647e19f064af37eb95389d0db7db6d244c99a
torus <torus@umich.com>
2017-01-24 14:31:06 -0500

    starter files

--- sql_startFiles/user.sql
+++ sql_startFiles/user.sql
@@ -1,4 +1,4 @@
 CREATE USER 'website'@'localhost' IDENTIFIED BY '<password>'
-GRANT SELECT,INSERT,UPDATE,DELETE ON codem.users TO 'website'@'localhost';
-GRANT SELECT,INSERT,UPDATE,DELETE ON codem.events TO 'website'@'localhost';
-GRANT SELECT,INSERT,UPDATE,DELETE ON codem.attendance TO 'website'@'localhost';
+GRANT SELECT,INSERT,UPDATE ON codem.users TO 'website'@'localhost';
+GRANT SELECT,INSERT,UPDATE ON codem.events TO 'website'@'localhost';
+GRANT SELECT,INSERT,UPDATE ON codem.attendance TO 'website'@'localhost';
